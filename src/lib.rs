21240e9971a2b894c84796b19cdce7f74c016c82
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-03 17:27:40 -0400

    Implement issuing commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 extern crate hiredis_sys as raw;
 extern crate libc;
 
+use libc::{c_char, c_int, size_t};
+use std::convert::{From, Into};
+use std::marker::PhantomData;
+
 macro_rules! raise(
     ($message:expr) => (return Err(::Error::from($message)));
 );
@@ -30,14 +34,104 @@ macro_rules! c_str_to_string(
     );
 );
 
+/// An argument.
+pub struct Argument {
+    pointer: *mut c_char,
+    size: size_t,
+}
+
+/// A context.
+pub struct Context {
+    raw: *mut raw::redisContext,
+    phantom: PhantomData<raw::redisContext>,
+}
+
+/// An error.
+#[derive(Debug)]
+pub struct Error {
+    pub message: Option<String>,
+}
+
+/// A reply.
+pub struct Reply {
+    raw: *mut raw::redisReply,
+    phantom: PhantomData<raw::redisReply>,
+}
+
 /// A result.
 pub type Result<T> = std::result::Result<T, Error>;
 
-mod context;
-mod error;
+impl<'l> From<&'l &'l str> for Argument {
+    #[inline]
+    fn from(data: &'l &'l str) -> Argument {
+        Argument {
+            pointer: data.as_ptr() as *mut u8 as *mut _,
+            size: data.len() as size_t,
+        }
+    }
+}
 
-pub use context::Context;
-pub use error::Error;
+impl Context {
+    /// Create a context by establishing connection to a server.
+    pub fn new(address: &str, port: usize) -> Result<Context> {
+        let context = Context {
+            raw: unsafe {
+                let raw = raw::redisConnect(str_to_c_str!(address), port as c_int);
+                if raw.is_null() {
+                    raise!("failed to create a context");
+                }
+                raw
+            },
+            phantom: PhantomData,
+        };
+        success!(context.raw);
+        Ok(context)
+    }
+
+    /// Issue a command.
+    pub fn command<'l, T>(&mut self, arguments: &'l [T]) -> Result<Reply>
+        where &'l T: Into<Argument>
+    {
+        let argc = arguments.len();
+        let mut argv = Vec::with_capacity(argc);
+        let mut argvlen = Vec::with_capacity(argc);
+        for argument in arguments.iter() {
+            let Argument { pointer, size } = argument.into();
+            argv.push(pointer);
+            argvlen.push(size);
+        }
+        let raw = unsafe {
+            raw::redisCommandArgv(self.raw, argc as c_int, argv.as_ptr() as *const *const _,
+                                  argvlen.as_ptr())
+        };
+        success!(self.raw);
+        debug_assert!(!raw.is_null());
+        Ok(Reply { raw: raw as *mut _, phantom: PhantomData })
+    }
+}
+
+impl Drop for Context {
+    #[inline]
+    fn drop(&mut self) {
+        unsafe { raw::redisFree(self.raw) };
+    }
+}
+
+impl<T> From<T> for Error where T: Into<String> {
+    #[inline]
+    fn from(message: T) -> Error {
+        Error {
+            message: Some(message.into()),
+        }
+    }
+}
+
+impl Drop for Reply {
+    #[inline]
+    fn drop(&mut self) {
+        unsafe { raw::freeReplyObject(self.raw as *mut _) };
+    }
+}
 
 /// Connect to a Redis server.
 #[inline]
--- tests/lib.rs
+++ tests/lib.rs
@@ -6,5 +6,6 @@ macro_rules! ok(
 
 #[test]
 fn workflow() {
-    let _redis = ok!(hiredis::connect("127.0.0.1", 4242));
+    let mut context = ok!(hiredis::connect("127.0.0.1", 4242));
+    let _reply = ok!(context.command(&[r#"SET foo "Hello, world!""#]));
 }
