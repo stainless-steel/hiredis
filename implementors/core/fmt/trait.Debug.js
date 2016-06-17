3e31b02784948590b7d4dc1db573de0328780e9d
Travis CI <>
2016-06-17 10:59:34 +0000

    Update the documentation

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = [];implementors["hiredis"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hiredis/struct.Error.html' title='hiredis::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='hiredis/enum.ErrorKind.html' title='hiredis::ErrorKind'>ErrorKind</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='hiredis/enum.Reply.html' title='hiredis::Reply'>Reply</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
